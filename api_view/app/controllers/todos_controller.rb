08a8f2cf56aaad82a2e8fdcbf74ee716ef04128e
Gaurav <gauravdev720@gmail.com>
2019-08-27 20:51:35 +0530

    First Commit

---
+++ api_view/app/controllers/todos_controller.rb
@@ -0,0 +1,25 @@
+class TodosController < ApplicationController
+
+	def index
+	end
+
+	def create
+		
+	end
+	
+	def show
+		
+	end
+
+	def edit
+		
+	end
+
+	def update
+		
+	end
+
+	def destroy
+		
+	end
+end
